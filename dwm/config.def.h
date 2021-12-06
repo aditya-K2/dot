ed44867d30182c5ed606f516a87e74668718ea31
aditya-K2 <adityakurdunkar2@gmail.com>
2021-12-06 16:12:44 +0530

    minor formatting

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -4,33 +4,33 @@
 
 /* appearance */
 
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 4;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 4;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-#define ICONSIZE 16   /* icon size */
-#define ICONSPACING 5 /* space between icon and title */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 0;       /* vertical padding of bar */
-static const int sidepad            = 0;      /* horizontal padding of bar */
-static const int DFW = 800;
-static const int DFH = 600;
-static const int DFX = 550;
-static const int DFY = 250;
-static const char *fonts[]          = { "SourceCodeProMedium:size=10" , "NotoColorEmoji:size=10"};
-static const char dmenufont[]       = "SourceCodeProMedium:size=10";
-static const char col_Selected_FG[]       = "#ffffff";
-static const char col_statusbarBG[] = "#111111";
-static const char col_statusbarMiddle[] = "#212121";
-static const char col_selectedBorder[] = "#212121";
-static const char col_notselectedBorder[] = "#212121";
-static const char col_statusbarFG[] = "#ffffff";
-static const char col_dmenuFG[] = "#bbbbbb";
+#define ICONSIZE 16                                   /* icon size */
+#define ICONSPACING 5                                 /* space between icon and title */
+static const unsigned int borderpx         =   0;     /* border pixel of windows */
+static const unsigned int gappx            =   4;     /* gaps between windows */
+static const unsigned int snap             =   32;    /* snap pixel */
+static const unsigned int systraypinning   =   1;     /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft    =   0;     /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing   =   4;     /* systray spacing */
+static const int systraypinningfailfirst   =   1;     /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray               =   1;     /* 0 means no systray */
+static const int showbar                   =   1;     /* 0 means no bar */
+static const int topbar                    =   1;     /* 0 means bottom bar */
+static const int vertpad                   =   0;     /* vertical padding of bar */
+static const int sidepad                   =   0;     /* horizontal padding of bar */
+static const int DFW                       =   800;
+static const int DFH                       =   600;
+static const int DFX                       =   550;
+static const int DFY                       =   250;
+static const char *fonts[]                 =   { "SourceCodeProMedium:size=10" , "NotoColorEmoji:size=10"};
+static const char dmenufont[]              =   "SourceCodeProMedium:size=10";
+static const char col_Selected_FG[]        =   "#ffffff";
+static const char col_statusbarBG[]        =   "#111111";
+static const char col_statusbarMiddle[]    =   "#212121";
+static const char col_selectedBorder[]     =   "#212121";
+static const char col_notselectedBorder[]  =   "#212121";
+static const char col_statusbarFG[]        =   "#ffffff";
+static const char col_dmenuFG[]            =   "#bbbbbb";
 
 
 static const char *colors[][3]      = {
