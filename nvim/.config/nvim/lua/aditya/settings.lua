2ef82cd02ff6ec61e9b33f64e8ea03f4b1843442
aditya-k2 <adityakurdunkar2@gmail.com>
2023-11-03 10:03:11 +0530

    WSL Changes

--- nvim/.config/nvim/lua/aditya/settings.lua
+++ nvim/.config/nvim/lua/aditya/settings.lua
@@ -8,18 +8,36 @@ vim.g["indent_blankline_filetype"]           = { "yaml", "lua" }
 vim.g["fzf_preview_window"]                  = {}
 vim.g["fzf_layout"]                          = { down = '15%' }
 
+if vim.fn.has("wsl") then
+    vim.cmd [[
+        let g:clipboard = {
+          \   'name': 'WslClipboard',
+          \   'copy': {
+          \      '+': 'clip.exe',
+          \      '*': 'clip.exe',
+          \    },
+          \   'paste': {
+          \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+          \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+          \   },
+          \   'cache_enabled': 0,
+          \ }
+    ]]
+end
+
 vim.opt.background="dark"
 vim.opt.termguicolors=true
 
 vim.cmd [[
     syntax enable
+    colorscheme base16-tomorrow-night
     syntax on
-    colorscheme darkblue
 ]]
 
 COLUMNS = 80
 -- vim.opt.colorcolumn=vim.fn.join(vim.fn.range(COLUMNS, 999),",")
 vim.opt.colorcolumn={COLUMNS}
+vim.opt.cmdheight=1
 vim.opt.hidden = true                                       -- Required to keep multiple buffers open multiple buffers
 vim.opt.wrap = false
 vim.opt.pumheight = 10                                      -- Makes popup menu smaller
@@ -43,6 +61,7 @@ vim.opt.smartindent = true
 vim.opt.incsearch = true
 vim.opt.list = true
 vim.opt.listchars = { eol = '↴', tab = '» ', trail = '~' --[[, space = "." ]] }
+vim.opt.fillchars = { vert = '|', horiz = '-' }
 vim.opt.compatible = false
 vim.opt.cursorline = true
 vim.opt.showmode = false
@@ -62,38 +81,38 @@ vim.cmd("sign define DiagnosticSignHint  texthl=DiagnosticSignHint  text=>"  )
 
 -- Diagnostics Highlights
 
--- vim.cmd( "hi DiagnosticsDefaultError guibg=NONE " )
--- vim.cmd( "hi DiagnosticVirtualTextError guifg=#ff3939 ctermbg=NONE gui=italic " )
--- vim.cmd( "hi DiagnosticVirtualTextWarn guifg=#ffa500 ctermbg=NONE gui=italic " )
--- vim.cmd( "hi DiagnosticVirtualTextHint guifg=#1d6a70 ctermbg=NONE gui=italic " )
--- vim.cmd( "hi DiagnosticFloatingError guifg=#ff3939 ctermbg=NONE gui=italic " )
--- vim.cmd( "hi DiagnosticFloatingWarn guifg=#ffa500 ctermbg=NONE gui=italic " )
--- vim.cmd( "hi DiagnosticFloatingHint guifg=#1d6a70 ctermbg=NONE gui=italic " )
--- vim.cmd( "hi DiagnosticSignError guifg=#ff3939 " )
--- vim.cmd( "hi DiagnosticSignHint guifg=#1d6a70 " )
+vim.cmd( "hi DiagnosticsDefaultError guibg=NONE " )
+vim.cmd( "hi DiagnosticVirtualTextError guifg=#ff3939 ctermbg=NONE gui=italic " )
+vim.cmd( "hi DiagnosticVirtualTextWarn guifg=#ffa500 ctermbg=NONE gui=italic " )
+vim.cmd( "hi DiagnosticVirtualTextHint guifg=#1d6a70 ctermbg=NONE gui=italic " )
+vim.cmd( "hi DiagnosticFloatingError guifg=#ff3939 ctermbg=NONE gui=italic " )
+vim.cmd( "hi DiagnosticFloatingWarn guifg=#ffa500 ctermbg=NONE gui=italic " )
+vim.cmd( "hi DiagnosticFloatingHint guifg=#1d6a70 ctermbg=NONE gui=italic " )
+vim.cmd( "hi DiagnosticSignError guifg=#ff3939 " )
+vim.cmd( "hi DiagnosticSignHint guifg=#1d6a70 " )
 
 -- Highlights
 
 vim.cmd( "hi Normal guibg=black" )
--- -- vim.cmd( "hi EndOfBuffer guibg=NONE" )
-vim.cmd( "hi LineNr guifg=#65a616" )
-vim.cmd( "hi CursorLineNr guifg=yellow guibg=None" )
--- vim.cmd( "hi SignColumn guibg=#262626" )
-vim.cmd( "hi NonText guibg=NONE guifg=#535e5a gui=italic" )
--- vim.cmd( "hi Pmenu guibg=#262526" )
-vim.cmd( "hi Comment guifg=grey gui=italic" )
--- -- vim.cmd( "hi Winbar guibg=#4e4e43" )
--- vim.cmd( "hi Statusline guibg=#1177aa guifg=white" )
--- vim.cmd( "hi WinbarNC guibg=#30302c" )
-vim.cmd( "hi MatchParen guifg=black guibg=orange")
-vim.cmd( "hi CursorLine guibg=#282f2d" )
--- vim.cmd( "hi CursorLineNr guibg=#111111" )
--- vim.cmd( "hi Visual guibg=#05233d" )
--- vim.cmd( "hi SpellBad guifg=#703435 gui=none " )
--- vim.cmd( "hi ColorColumn guibg=darkred" )
-vim.cmd( "hi String guifg=#89b3d9" )
-vim.cmd( "hi Identifier guifg=#ececec" )
--- vim.cmd( "hi Function guifg=#deb03e" )
-vim.cmd( "hi @method.call guifg=#90fff0" )
--- vim.cmd("hi @parameter guibg=None guifg=grey")
-vim.cmd( "hi WinSeparator guibg=None" )
+-- -- -- vim.cmd( "hi EndOfBuffer guibg=NONE" )
+-- vim.cmd( "hi LineNr guifg=#65a616" )
+-- vim.cmd( "hi CursorLineNr guifg=yellow guibg=None" )
+-- -- vim.cmd( "hi SignColumn guibg=#262626" )
+vim.cmd( "hi NonText guifg=#454545" )
+-- -- vim.cmd( "hi Pmenu guibg=#262526" )
+-- vim.cmd( "hi Comment guifg=grey gui=italic" )
+-- -- -- vim.cmd( "hi Winbar guibg=#4e4e43" )
+-- -- vim.cmd( "hi Statusline guibg=#1177aa guifg=white" )
+-- -- vim.cmd( "hi WinbarNC guibg=#30302c" )
+-- vim.cmd( "hi MatchParen guifg=black guibg=orange")
+-- vim.cmd( "hi CursorLine guibg=#282f2d" )
+-- -- vim.cmd( "hi CursorLineNr guibg=#111111" )
+-- -- vim.cmd( "hi Visual guibg=#05233d" )
+-- -- vim.cmd( "hi SpellBad guifg=#703435 gui=none " )
+-- -- vim.cmd( "hi ColorColumn guibg=darkred" )
+-- vim.cmd( "hi String guifg=#89b3d9" )
+-- vim.cmd( "hi Identifier guifg=#ececec" )
+-- -- vim.cmd( "hi Function guifg=#deb03e" )
+-- vim.cmd( "hi @method.call guifg=#90fff0" )
+-- -- vim.cmd("hi @parameter guibg=None guifg=grey")
+-- vim.cmd( "hi WinSeparator guibg=None" )
