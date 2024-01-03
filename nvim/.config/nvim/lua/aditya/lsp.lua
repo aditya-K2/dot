cb7ca8b13e3063262e153079f1ad3bb6ead22c34
aditya-k2 <adityakurdunkar2@gmail.com>
2024-01-03 10:49:45 +0530

    lsp: HTML Server

--- nvim/.config/nvim/lua/aditya/lsp.lua
+++ nvim/.config/nvim/lua/aditya/lsp.lua
@@ -101,8 +101,10 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 require'lspconfig'.cssls.setup {
   capabilities = capabilities,
+  on_attach= on_attach
 }
 require'lspconfig'.html.setup {
   capabilities = capabilities,
+  on_attach = on_attach
 }
 -- require "lsp_signature".setup()
