f880ad9ea1638293837e1ba1c0fd7a1166f4f88e
aditya-K2 <adityakurdunkar2@gmail.com>
2024-02-05 16:09:51 +0530

    lazy: Ensure required mason packages are installed

--- nvim/.config/nvim/lua/aditya/lazy.lua
+++ nvim/.config/nvim/lua/aditya/lazy.lua
@@ -11,6 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local ensure_installed_mason_packages = {
+    "clangd",
+    "yaml-language-server",
+    "vim-language-server",
+    "gopls"
+}
+
 require("lazy").setup({
     'tpope/vim-commentary',
     -- 'mattn/emmet-vim',
@@ -75,6 +82,18 @@ require("lazy").setup({
     'neovim/nvim-lspconfig',
     { 'williamboman/mason.nvim', config = function()
         require("mason").setup()
+
+        -- Ensure required packages are installed.
+        local registry = require "mason-registry"
+        registry.refresh(function ()
+            for _, pkg_name in ipairs(ensure_installed_mason_packages) do
+                local pkg = registry.get_package(pkg_name)
+                if not pkg:is_installed() then
+                    pkg:install()
+                end
+            end
+        end)
+
     end },
     { 'williamboman/mason-lspconfig.nvim', config = function()
         require("mason-lspconfig").setup()
