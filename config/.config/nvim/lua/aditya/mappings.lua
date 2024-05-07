f5cb3e9e84a36c146b93bb40215e2aa317edfbb1
aditya-K2 <adityakurdunkar2@gmail.com>
2024-05-07 22:03:30 +0530

    nvim.mappings: Cleanup

--- config/.config/nvim/lua/aditya/mappings.lua
+++ config/.config/nvim/lua/aditya/mappings.lua
@@ -1,55 +1,8 @@
 vim.g["mapleader"] = " " -- set leader key
-math.randomseed(os.time())
-math.random(); math.random(); math.random()
-
-local function nnoremap(lhs, rhs)
-    vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
-end
-
-local function nmap(lhs, rhs)
-    vim.api.nvim_set_keymap("n", lhs, rhs, {})
-end
-
-local function vnoremap(lhs, rhs)
-    vim.api.nvim_set_keymap("v", lhs, rhs, { noremap = true })
-end
-
-local function format_func(formatter)
-    return function()
-        if vim.api.nvim_buf_get_option(0, "modified") then
-            local cpos = vim.fn.getpos('.')
-            local file = vim.fn.expand("%")
-            vim.fn.jobstart({ formatter, file }, {
-                stdout_buffered = true,
-                on_stdout = function(_, data)
-                    if data then
-                        vim.api.nvim_buf_set_lines(0, 0, -1, false, data)
-                        vim.fn.setpos('.', cpos)
-                        vim.api.nvim_command("noautocmd write")
-                    end
-                end
-            })
-        end
-    end
-end
-
-local function format_buffer()
-    vim.lsp.buf.format()
-end
 
 local format_group = vim.api.nvim_create_augroup("__aditya__format_group", { clear = true })
 local c_files = { "*.cpp", "*.cc", "*.c" }
-local show_spaces = { "*.yaml", "*.py" }
 local notes = { "*/notes", "*/note" }
-local js = { "*.js", "*.jsx", "*.ts", "*.html", "*.tsx", "*.css" }
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    group = format_group,
-    pattern = show_spaces,
-    callback = function()
-        vim.cmd("setlocal listchars+=space:.")
-    end,
-})
 
 vim.api.nvim_create_autocmd("BufEnter", {
     group = format_group,
@@ -71,25 +24,9 @@ vim.api.nvim_create_autocmd("BufEnter", {
 
 vim.api.nvim_create_autocmd("BufEnter", {
     group = format_group,
-    pattern = js,
-    callback = function()
-        vim.api.nvim_buf_set_keymap(0, "n", "<leader>f", ":Prettier<CR>", { noremap = true, silent = false })
-    end
-})
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    group = format_group,
     pattern = notes,
     callback = function()
         vim.cmd("set filetype=markdown")
-    end
-})
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    group = format_group,
-    pattern = { "*.tf" },
-    callback = function()
-        vim.cmd("set filetype=hcl")
     end
 })
 
@@ -108,50 +45,42 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
 })
 
+local function nnoremap(lhs, rhs)
+    vim.api.nvim_set_keymap("n", lhs, rhs, { noremap = true, silent = true })
+end
+
+local function nmap(lhs, rhs)
+    vim.api.nvim_set_keymap("n", lhs, rhs, {})
+end
+
+local function vnoremap(lhs, rhs)
+    vim.api.nvim_set_keymap("v", lhs, rhs, { noremap = true })
+end
 
 nnoremap("<TAB>", ":bnext<CR>")
 nnoremap("<S-TAB>", ":bprevious<CR>")
 nnoremap("Y", "y$")
 nnoremap("<C-\\>", ":vsplit <CR>")
 nnoremap("<leader><CR>", ":split<CR>")
-nnoremap("<leader>fc", "<cmd> lua require('telescope.builtin').find_files{cwd='/H/code', prompt='cpFiles'}<CR>")
 nnoremap("<leader>m", ":MaximizerToggle <CR>")
-nnoremap("U", "<cmd>lua vim.diagnostic.open_float(0, { scope = \"line\" })<CR>")
 
--- Split Mappings
+-- Split Navigation Mappings
 
 nnoremap("<M-h>", "<C-w>h")
 nnoremap("<M-j>", "<C-w>j")
 nnoremap("<M-k>", "<C-w>k")
 nnoremap("<M-l>", "<C-w>l")
 
-vim.cmd('noremap <Leader>y "*y')
-vim.cmd('noremap <Leader>p "*p')
+-- Clipboard
 
-vim.cmd("autocmd BufRead,BufNewFile *.txt nmap <CR> :wq <CR>")
-vim.cmd("autocmd BufWritePre * :%s/\\s\\+$//e")
-vim.cmd("autocmd BufRead,BufNewFile .zshrc set filetype=bash")
-vim.cmd("autocmd BufRead,BufNewFile *.html set noexpandtab")
-
-vim.cmd("autocmd BufRead,BufNewFile *.md set wrap")
-vim.cmd("autocmd BufRead,BufNewFile *.md nnoremap <C-o> :call Spellfloat()<CR>")
-vim.cmd(
-"autocmd BufRead,BufNewFile *.md  nmap <leader>mm :!pandoc % -o output.pdf --highlight-style breezedark && zathura output.pdf &<CR>")
-vim.cmd("autocmd BufRead,BufNewFile *.md nmap <leader>mc :!pandoc % -o output.pdf --highlight-style breezedark &<CR>")
-
-vim.cmd("autocmd BufRead,BufNewFile *.latex set wrap")
-vim.cmd("autocmd BufRead,BufNewFile *.latex nnoremap <C-o> :call Spellfloat()<CR>")
-vim.cmd("autocmd BufRead,BufNewFile *.latex nmap <leader>mm :!compileLatex % <CR>")
-vim.cmd("autocmd BufRead,BufNewFile *.latex nmap <leader>mc :!pdflatex -shell-escape % <CR>")
+nnoremap('<Leader>y','*y')
+nnoremap('<Leader>p','*p')
 
 nmap("<leader>fn", ":Files ~/.config/nvim/<CR>")
 nmap("<leader>fs", ":Files<CR>")
 nmap("<leader>fo", ":Buffers<CR>")
 nmap("<leader>fg", ":Rg <CR>")
 nmap("<leader>fh", ":Helptags <CR>")
-
-vim.api.nvim_set_keymap("i", "<C-BS>", "<C-w>", {})
-vim.api.nvim_set_keymap("i", "<C-h>", "<C-w>", {})
 
 -- Better tabbing
 
@@ -162,3 +91,11 @@ vnoremap(">", ">gv")
 
 vnoremap("J", ":m '>+1<CR>gv=gv")
 vnoremap("K", ":m '<-2<CR>gv=gv")
+
+vim.api.nvim_set_keymap("i", "<C-BS>", "<C-w>", {})
+vim.api.nvim_set_keymap("i", "<C-h>", "<C-w>", {})
+
+vim.cmd("autocmd BufRead,BufNewFile *.txt nmap <CR> :wq <CR>")
+vim.cmd("autocmd BufWritePre * :%s/\\s\\+$//e")
+vim.cmd("autocmd BufRead,BufNewFile .zshrc set filetype=bash")
+vim.cmd("autocmd BufRead,BufNewFile *.html set noexpandtab")
