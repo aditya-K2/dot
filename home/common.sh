ca930a9477256182bbc37fe32a0481d0610bd914
aditya-k2 <adityakurdunkar2@gmail.com>
2024-10-25 11:31:24 +0530

    common.sh: Add epyenv

--- home/common.sh
+++ home/common.sh
@@ -220,6 +220,12 @@ envm() {
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
 
+epyenv() {
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+}
+
 sb() {
     if [[ -n "$ZSH_VERSION" ]]; then
         source ~/.zshrc
